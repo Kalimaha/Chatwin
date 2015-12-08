cd53d403f5e45d3e467704a750f8f2a215bb2263
Kalimaha <guido.barbaglia@gmail.com>
2015-12-08 14:15:36 +0100

    Background.

--- src/js/both/commons.js
+++ src/js/both/commons.js
@@ -3,13 +3,16 @@ Router.configure({
 });
 
 Router.route('/', {
+    name: 'carousel',
     template: 'carousel_page'
 });
 
 Router.route('/login', {
+    name: 'login',
     template: 'login_page'
 });
 
 Router.route('/create/activity', {
+    name: 'create_activity',
     template: 'create_activity_page'
 });
