b17a22e97d3a7b607d729b5b0bcd12192c21292c
Kalimaha <guido.barbaglia@gmail.com>
2015-12-21 17:52:27 +0100

    Work in progress: create activity.

--- src/js/both/routes.js
+++ src/js/both/routes.js
@@ -85,16 +85,9 @@
             Meteor.subscribe('events');
         },
         data: function () {
-            if (Session.get('user') === undefined) {
-                Router.go('login');
-            } else if (Session.get('event_id') === undefined) {
-                Session.set('errorMessage', 'Event ID is undefined');
-                Router.go('error');
-            } else {
-                return {
-                    single_activities: Meteor.Events.find({_id: Session.get('event_id')}, {_id: 0, activities: 1})
-                };
-            }
+            return {
+                single_activities: Meteor.Events.find({_id: Session.get('event_id')}, {_id: 0, activities: 1})
+            };
         }
     });
 
