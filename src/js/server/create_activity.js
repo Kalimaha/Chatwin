ae084d6381cd97bd2ad2fc6d07a20655df91c89c
Kalimaha <guido.barbaglia@gmail.com>
2016-01-22 16:12:01 +0100

    Fixed bug in currency conversion.

--- src/js/server/create_activity.js
+++ src/js/server/create_activity.js
@@ -5,20 +5,18 @@
 
     Meteor.methods({
         exchange_currency: function (value, default_currency, currency) {
-            console.log(value);
-            console.log(default_currency);
-            console.log('to');
-            console.log(currency);
-            var url = 'http://api.fixer.io/latest?symbols=' + default_currency + ',' + currency,
-                result = HTTP.get(url),
-                out;
-            out = value * result.data.rates[default_currency];
-            if (isNaN(out)) {
-                console.log(value);
+            if (default_currency === currency) {
                 return value;
+            } else {
+                var url = 'http://api.fixer.io/latest?symbols=' + default_currency + ',' + currency,
+                    result = HTTP.get(url),
+                    out;
+                out = value * result.data.rates[default_currency];
+                if (isNaN(out)) {
+                    return value;
+                }
+                return out;
             }
-            console.log(out);
-            return out;
         }
     });
 
