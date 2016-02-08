9947bc89a4003921825841745582ddd790886234
Kalimaha <guido.barbaglia@gmail.com>
2016-02-08 09:06:58 +0100

    Login fixed.

--- src/js/routing/home.js
+++ src/js/routing/home.js
@@ -10,11 +10,14 @@
         onBeforeAction: function () {
             var u = Meteor.user();
             if (u === null || u.services === null) {
+                console.log('login 1');
                 return Router.go('login');
             }
             if (u.services === undefined || (u.services.facebook === undefined && u.services.google === undefined)) {
+                console.log('login 2');
                 return Router.go('login');
             } else {
+                console.log('events');
                 return Router.go('events');
             }
         }
--- src/js/routing/login.js
+++ src/js/routing/login.js
@@ -6,22 +6,7 @@
     Router.route('/login', {
         name: 'login',
         template: 'login_page',
-        layoutTemplate: null,
-        waitOn: function () {
-            Meteor.subscribe('friends');
-            return Meteor.subscribe('getUserData');
-        },
-        onBeforeAction: function () {
-            var u = Meteor.user();
-            if (u === null || u.services === null) {
-                return Router.go('login');
-            }
-            if (u.services === undefined || (u.services.facebook === undefined && u.services.google === undefined)) {
-                return Router.go('login');
-            } else {
-                return this.next();
-            }
-        }
+        layoutTemplate: null
     });
 
 }());
